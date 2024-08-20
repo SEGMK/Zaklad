d5ab400e497c8ed35f58d7484a437c9d4047a661
SEGMK <85564914+SEGMK@users.noreply.github.com>
2024-08-20 19:48:17 +0200

    Fixed doubling product values

--- Zaklad/ViewModel/ProductEditorViewModel_UserProduct.cs
+++ Zaklad/ViewModel/ProductEditorViewModel_UserProduct.cs
@@ -30,16 +30,17 @@ namespace Zaklad.ViewModel
         }
 
         public int Gramature { get => UserProduct.Gramature; set { UserProduct.Gramature = value; OnPropertyChange(nameof(Gramature)); } }
-        public decimal EditableKcal { get => UserProduct.Kcal; set { UserProduct.ProductTemplate.Kcal = value; OnPropertyChange(nameof(EditableKcal)); } }
-        public decimal EditableProteins { get => UserProduct.Proteins; set { UserProduct.ProductTemplate.Proteins = value; OnPropertyChange(nameof(EditableProteins)); } }
-        public decimal EditableFat { get => UserProduct.Fat; set { UserProduct.ProductTemplate.Fat = value; OnPropertyChange(nameof(EditableFat)); } }
-        public decimal EditableCarbohydrates { get => UserProduct.Carbohydrates; set { UserProduct.ProductTemplate.Carbohydrates = value; OnPropertyChange(nameof(EditableCarbohydrates)); } }
+        public decimal EditableKcal { get => UserProduct.Kcal; set { UserProduct.ProductTemplate.Kcal = GetProductTemplateValue(value); OnPropertyChange(nameof(EditableKcal)); } }
+        public decimal EditableProteins { get => UserProduct.Proteins; set { UserProduct.ProductTemplate.Proteins = GetProductTemplateValue(value); OnPropertyChange(nameof(EditableProteins)); } }
+        public decimal EditableFat { get => UserProduct.Fat; set { UserProduct.ProductTemplate.Fat = GetProductTemplateValue(value); OnPropertyChange(nameof(EditableFat)); } }
+        public decimal EditableCarbohydrates { get => UserProduct.Carbohydrates; set { UserProduct.ProductTemplate.Carbohydrates = GetProductTemplateValue(value); OnPropertyChange(nameof(EditableCarbohydrates)); } }
         public string ProductName { get => UserProduct.Name; set { UserProduct.ProductTemplate.Name = value; OnPropertyChange(nameof(ProductName)); } }
 
         public ImageSource ProductImage => UserProduct.ProductImage;
 
         public event PropertyChangedEventHandler? PropertyChanged;
         private void OnPropertyChange(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        private decimal GetProductTemplateValue(decimal value) => value / (Gramature / 100);
         public ProductEditorViewModel_UserProduct(IUserProduct product)
         {
             UserProduct = product;
